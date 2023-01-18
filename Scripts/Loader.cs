6209d8c1d93785f5d8b957868bbe494d3f962f43
BuildTools <unconfigured@null.spigotmc.org>
2023-01-18 13:15:48 +0100

    update to beta version

--- Scripts/Loader.cs
+++ Scripts/Loader.cs
@@ -2,13 +2,15 @@
 
 namespace MR223_plugin
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("pl.szikaka.receiver_2_modding_kit")]
+    [BepInPlugin("Ciarencew.MR223", PluginInfo.PLUGIN_NAME, "0.1.0")]
     public class Loader : BaseUnityPlugin
     {
+        public static readonly string folder_name = "MR223_Files";
         private void Awake()
         {
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin Ciarencew.MR223 is loaded!");
         }
     }
 }
