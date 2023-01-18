805e7c86e9a52a8d06e2979fc29e98d1ca7f59d9
BuildTools <unconfigured@null.spigotmc.org>
2023-01-18 21:57:49 +0100

    Update to 1.1.0

--- Scripts/Loader.cs
+++ Scripts/Loader.cs
@@ -3,7 +3,7 @@
 namespace MR223_plugin
 {
     [BepInDependency("pl.szikaka.receiver_2_modding_kit")]
-    [BepInPlugin("Ciarencew.MR223", PluginInfo.PLUGIN_NAME, "1.0.0")]
+    [BepInPlugin("Ciarencew.MR223", PluginInfo.PLUGIN_NAME, "1.1.0")]
     public class Loader : BaseUnityPlugin
     {
         public static readonly string folder_name = "MR223_Files";
